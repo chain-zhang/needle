340adb93a990ecb1d286fc2a5d84acc582e39652
ChainZhang <zzceng@126.com>
2024-07-26 18:38:44 +0800

    add ads

--- js/HardestGame.js
+++ js/HardestGame.js
@@ -202,16 +202,15 @@
             }
         },
         showAd:function () {
-            console.log('show ad');
-            // AdController.show().then((result) => {
-            //     // user watch ad till the end
-            //     // your code to reward user
-            //     alert('Reward');
-            // }).catch((result) => {
-            //     // user skipped video or get error during playing ad
-            //     // do nothing or whatever you want
-            //     alert(JSON.stringify(result, null, 4));
-            // })
+            AdController.show().then((result) => {
+                // user watch ad till the end
+                // your code to reward user
+                alert('Reward');
+            }).catch((result) => {
+                // user skipped video or get error during playing ad
+                // do nothing or whatever you want
+                alert(JSON.stringify(result, null, 4));
+            })
         },
         update:function (){
             if(!this.isPause&&!this.isOver){
